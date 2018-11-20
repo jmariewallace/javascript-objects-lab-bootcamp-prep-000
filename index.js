0bc04457e3214566b5ace2b47c3821bf5d23493e
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-20 23:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object.key
+  var newObj = Object.assign({}, object, key)
+  return delete newObj.key
 }
