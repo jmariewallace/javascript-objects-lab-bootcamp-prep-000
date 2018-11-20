959d74e2c72c7b0477f5f22a53a0338abfd12dbb
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-20 23:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 /*Objects defines a 'recipes' object*/
 var recipes = {}
 
-/*Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair*//*Objects updateObjectWithKeyAndValue(object, key, value) it does not modifythe original object, but rather returns a clone with the new data*/
+/*Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pairs & Objects updateObjectWithKeyAndValue(object, key, value) it does not modifythe original object, but rather returns a clone with the new data*/
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
-
-
-
-
-
 
