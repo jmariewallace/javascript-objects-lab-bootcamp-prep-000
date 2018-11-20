1189a068ecf1dc542a490075c8402f755ec5ac5d
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-20 23:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 /*Objects updateObjectWithKeyAndValue(object, key, value) it does not modifythe original object, but rather returns a clone with the new data*/
 function updateObjectWithKeyAndValue(object, key, value) {
-return [...object, key: value]
+  return Object.push(object, key, value)
 }
 
 
